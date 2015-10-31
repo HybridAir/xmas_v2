ad79bcc104cdfe7cb65c9b9ffc725a322bbfb795
HybridAir <s.tratton.blake@gmail.com>
2015-10-31 15:01:54 -0700

    Basic blinky stuff

--- software/mainProgram/mainProgram.ino
+++ software/mainProgram/mainProgram.ino
@@ -1,9 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+//I want to blink on pb4
 
+
+void setup() {
+  // initialize digital pin 13 as an output.
+  DDRB |= (1<<PB4);			//make pb4 an output
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  PORTB |= (1<<PB4);
+  delay(1000);              // wait for a second
+  PORTB &= ~(1<<PB4);
+  delay(1000);              // wait for a second
 }
