14862d8e3ee2464d40b9b7aa426afdfd7f6c1af8
HybridAir <s.tratton.blake@gmail.com>
2015-10-31 19:47:03 -0700

    I managed to get io on the reset pin working so that's something

--- software/mainProgram/mainProgram.ino
+++ software/mainProgram/mainProgram.ino
@@ -3,13 +3,13 @@
 
 void setup() {
   // initialize digital pin 13 as an output.
-  DDRB |= (1<<PB4);			//make pb4 an output
+  DDRB |= (1<<PB5);			//make pb4 an output
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  PORTB |= (1<<PB4);
+  PORTB |= (1<<PB5);
   delay(1000);              // wait for a second
-  PORTB &= ~(1<<PB4);
+  PORTB &= ~(1<<PB5);
   delay(1000);              // wait for a second
 }
