4f973084244422c5f3e065a35efd4bc7db478a6e
HybridAir <s.tratton.blake@gmail.com>
2015-11-04 15:10:43 -0700

    Moved the led code over to where it belongs

--- software/mainProgram/charlieplexer.ino
+++ software/mainProgram/charlieplexer.ino
@@ -43,3 +43,95 @@ void charmander(byte i) {
 	PORTB = (1<<charliePin[led1]);
   
 }
+
+
+
+//displays the currently selected animation on the charlieplexed LEDs
+void animation() {
+	
+	//if(currentAnim <= 3) {								//if animations 0-3 are selected
+	if(currentAnim <= 5) {								//if animations 0-3 are selected
+	
+		if(currentFrame >= MAXLEDS) {					//used for looping the animation
+			currentFrame = 0;
+		}
+		
+		byte ledOut = currentFrame;						//get the LED that needs to be lit
+		
+		if(currentAnim % 2 == 0) {						//if animations 0 or 2 are selected, reverse the order that the LEDs light up
+			ledOut = (MAXLEDS - 1) - currentFrame;
+		}
+		charmander(ledOut);								//light up a single LED
+		
+
+		if(currentAnim > 1) {							//if animations 2 or 3 are selected
+
+			//add two extra LEDs, offset by 6 each to create a sort of triangle effect
+
+			for(char i = 0;i<2;i++) {							//need to add two extra LEDs
+				byte offset = 6 + (6*i);						//create the offset
+				byte led = ledOut + offset;						//get the new led number to light
+				if(led >= MAXLEDS) {							//check for overflows
+					led = led - MAXLEDS;						//loop the led to the beginning if needed
+				}
+				charmander(led);								//light the LED
+			}
+		}
+	}
+	else if(currentAnim == 6) {							//blink each full color section at once
+	
+		if(currentFrame > 2) {
+			currentFrame = 0;
+		}
+
+		for(byte x = 0;x < MAXLEDS;x++) {		//go through every led, starting from the color offset
+			if(x % 3 == 0) {					//only match every third led
+				byte outled = x + currentFrame;
+				if(outled < MAXLEDS) {
+					charmander(outled);					//light the led we got, plus the color offset
+				}
+			}
+	
+		}
+
+	}
+	else {
+		if(currentFrame % 8 == 0) {
+			if(currentColor) {
+				currentColor = 0;
+			}
+			else {
+				currentColor = 1;
+			}
+			currentFrame = 1;
+		}
+		
+		if(currentColor) {
+			for(byte i=0;i < MAXLEDS;i++) {
+				charmander(i);
+			}
+		}
+		else {
+			DDRB = 0;
+			PORTB = 0;
+		}
+	}
+}
+
+
+void checkFrame() {
+	if(millis() > FRAME_DELAY + lastFrameTime) {
+		lastFrameTime = millis();
+		currentFrame++;
+	}
+}
+
+
+void switchAnimation() {
+	currentFrame = 0;
+	
+	currentAnim++;
+	if(currentAnim > 7) {
+		currentAnim = 0;
+	}
+}
